7070099cd2756568a294ea738318951006443f2e
Alex Anderson <aga@berkeley.edu>
2015-07-15 00:17:32 -0700

    first commit of sparse coding tutorial

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RCTN-neuro-bootcamp
 Repo for the RCTN section of the HWNI Neuroscience Boot Camp
+
+This repo contains the files for the Redwood center's contribution to the HWNI neuro boot camp.
---
+++ utils/rf_plot.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def show_fields(M, cmap = plt.cm.bwr, m = None, pos_only = False):
+    """
+    M - dictionary with dict elements as rows (each row is an unraveled image)
+    cmap - color map for plot
+    m - Plot a m by m grid of receptive fields
+    Plots receptive fields
+    """
+    N, N_pix = M.shape
+    if (m == None):
+        m = int(np.sqrt(N - 0.01)) + 1
+    
+    l = int(np.sqrt(N_pix)) # Linear dimension of the image
+    
+    mm = np.max(np.abs(M))
+    
+    out = np.zeros(((l + 1) * m, (l + 1) * m)) + np.max(M)
+
+    for u in range(N):
+        i = u / m
+        j = u % m
+        out[(i * (l + 1)):(i * (l + 1) + l), (j * (l + 1)):(j * (l + 1) + l)] = np.reshape(M[u], (l, l))
+        
+        
+    if pos_only:
+        m0 = 0
+    else:
+        m0 = -mm
+    m1 = mm
+    #plt.imshow(out, cmap = plt.cm.gray_r, interpolation = 'nearest')
+    plt.imshow(out, cmap = cmap, interpolation = 'nearest',
+               vmin = m0, vmax = m1)
+    plt.colorbar()
