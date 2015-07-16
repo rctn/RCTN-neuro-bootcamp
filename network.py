52cfaef47c0ab9b72ee8247dd4a3b86b8d74168c
Alex Anderson <aga@berkeley.edu>
2015-07-16 13:01:14 -0700

    updates to fistaand ista functions, still need to test

--- network.py
+++ network.py
@@ -11,35 +11,71 @@ def calculate_L(D):
     D : array
     """
     try:
-        L = 2 * sp.linalg.eigh(np.dot(D, D.T), eigvals_only=True, eigvals=(N_sp-1,N_sp-1))[0]
+        L = 2 * sp.linalg.eigh(np.dot(D, D.T), 
+                               eigvals_only=True, 
+                               eigvals=(N_sp-1,N_sp-1))[0]
     except ValueError:
-        L = (2 * std ** 2 * D.shape[1]) # Upper bound on largest eigenvalue
-               # std - standard deviation of the dictionary elements
+        L = (2 * D.shape[1])
     return L
 
-def fista(I_idx, Alpha, L, N_g_itr, X0, print_costs = False):
+def fista(cost, grad_A, A0, n_steps, l, print_costs = False):
     """
     Computes FISTA Approximation
+
+    Parameters
+    ----------
+    cost : function handle
+        Function handle that gives the cost given the sparse coefficients, A
+    grad_A: function handle
+        Function that takes in the sparse coefficients 
+        and gives dE_reconstruction_error/dA
+    A0 : matrix
+        Initial value for sparse coefficents
+    n_steps : int
+        Number of iterations
+    l : float
+        Lipschitz constant for grad_A
     """
-    Xs = np.zeros((2, N_bat, N_sp)).astype('float32')
-    Ys = np.zeros((2, N_bat, N_sp)).astype('float32')
+    n_bat, n_sp = A0.shape
+    Xs = np.zeros((2, n_bat, n_sp))
+    Ys = np.zeros((2, n_bat, n_sp))
     Ts = np.zeros((2,)).astype('float32')
-    Xs[0] = X0
-    Ys[0] = X0
+    Xs[0] = A0
+    Ys[0] = A0
     Ts[0] = 1.
-    for i in range(N_g_itr-1):
+    for i in range(n_steps-1):
         i0 = i % 2
         i1 = i % 2
-        t_A.set_value(Ys[i0])
-        E, E_rec, E_sp, SNR = costs(I_idx, Alpha)
-        if (i % (N_g_itr/10) == 0) and print_costs:
-            print E / N_bat, E_rec / N_bat, E_sp / N_bat, SNR
-        Xs[i1] = ista(I_idx, Alpha, L)[0]
-        #        coeff_thresh()
+        A = Ys[i0]
+        Xs[i1] = ista(A, grad_A(A),  alpha, l)
         Ts[i1] = 0.5 * (1. + np.sqrt(1. + 4. * Ts[i0] ** 2))
         Ys[i1] = Xs[i1] + (Ts[i0]-1)/ Ts[i1] * (Xs[i1]-Xs[i0])
+        E, E_rec, E_sp, SNR = cost(A)
+        if (i % (N_g_itr/10) == 0) and print_costs:
+            print E / N_bat, E_rec / N_bat, E_sp / N_bat, SNR
 
-def normalize(D):
+def ista(A, gradA, alpha, l):
+    """
+    Returns the new value of the sparse coefficients after one ista step
+        for the cost function f(A) + Alpha * |A|
+    Parameters
+    ----------
+    A : array
+        Current value of sparse coefficients
+    gradA : array
+        Derivative of df(A)/dA
+    alpha : float
+        Parameter for regularization term
+    l : float
+        Lipschitz constant for dE_rec/dA
+    """
+    theta = alpha / l
+    Ap = A - 1./l * gradA
+    max = 1000000. #FIXME better function than clip?
+    return np.sign(Ap) * np.clip(np.abs(Ap) - theta, 0., max)
+
+def row_normalize(D):
+    # FIXME is this right?
     norms = np.sqrt(np.diag((D**2).sum(0)))
     D = D.dot(np.diag(1./norms))
     return D
@@ -53,7 +89,7 @@ class Network(object):
     n_dict : int
         Number of dictionary elements.
     n_features : int
-        Number (dimensionality) of features.
+        Number (dimensionality) of features (eg. number of pixels)
     lamb : float
         Sparsity coefficient.
     eta : float
@@ -64,9 +100,9 @@ class Network(object):
         self.n_feature = n_features
         self.lamb = lamb
         self.eta = eta
-        self.activities = None
+        self.activities = None # ??
         self.reset()
-        self.stale_A = True
+        self.stale_A = True  # ??
 
     def reset(self, n_dict=None, n_features=None):
         """
@@ -81,6 +117,7 @@ class Network(object):
             n_dict = self.n_dict
         if n_features is None:
             n_features = self.n_features
+        # Fix for positive only
         self.D = normalize(np.random.randn((n_features,
                                             n_dict)))
 
@@ -91,37 +128,39 @@ class Network(object):
         Parameters
         ----------
         X : array
-            Input data.
+            A batch of input data.
         n_steps : int
-            Number of steps for inferences.
+            Number of steps for inference.
         """
         self.stale_A = False
         self.X = X
         cost = functools.partial(self.cost, X)
         grad = functools.partial(self.grad_A, X)
-        A = fista(cost, grad_A, A, n_steps, track_cost)
+        l = calculate_l(self.D)
+        A0 = np.zeros_like(A)
+        A = fista(cost, grad_A, A0, n_steps, track_cost)
         if isinstance(A, tuple):
-            self.A, cost = A
+            self.A, cost = A #???
         else:
             self.A = A
         return A
 
     def grad_A(self, X, A):
         """
-        Gradient of the cost w.r.t. A.
+        Gradient of the reconstruction error w.r.t. A.
 
         Parameters
         ----------
         X : array
-            Input data.
+            A batch of input data.
         A : array 
             Sparse coefficients.
         """
-        return -(X-A.dot(self.D)).dot(D.T)
+        return -(X-A.dot(self.D)).dot(self.D.T)
 
     def grad_D(self, X, A):
         """
-        Gradient of the cost w.r.t. D.
+        Gradient of the reconstruction error w.r.t. D.
 
         Parameters
         ----------
@@ -130,7 +169,7 @@ class Network(object):
         A : array 
             Sparse coefficients.
         """
-        return -D.dot(X-A.dot(self.D).T)+self.lamb*np.sign(A)
+        return -(A.T).dot(X-A.dot(self.D))
 
     def learn_D(self, X=None, A=None):
         """
@@ -150,17 +189,17 @@ class Network(object):
             X = self.X
         if A is None:
             A = self.A
-        self.D = normalize(D-self.eta*self.grad_D(X, A))
+        self.D = normalize(D-self.eta*normalize(self.grad_D(X, A)))
         self.stale_A = True
 
-    def train(self, X, batch_size=100, n_epochs=10, reset=True):
+    def train(self, data, batch_size=100, n_epochs=10, reset=True):
         """
         Train a dictionary: D, on the data: X.
 
         Parameters
         ----------
-        X : array
-            Input data.
+        data : array
+            Input data. (n_examples, n_features)
         batch_size : int  (optional)
             Batch size for training.
         n_epochs : int (optional)
@@ -168,13 +207,14 @@ class Network(object):
         reset : boolean (optional)
             Reset dictionary before training.
         """
-        self.reset()
+        if reset:
+            self.reset()
         batch_size = min(batch_size, X.shape[0])
         n_batches = math.ceil(X.shape[0]/batch_size)
         for ii in range(n_epochs):
             order = np.random.permutation(n_batches)
             for jj in order:
-                batch = X[jj*batch_size:min((jj+1)*batch_size)]
+                batch = data[jj*batch_size:min((jj+1)*batch_size)]
                 A = self.infer_A(batch)
                 self.learn_D()
             print("Epoch "+str(ii)+" of "+str(n_epochs))
